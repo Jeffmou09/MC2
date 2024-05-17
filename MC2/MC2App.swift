913331c241f92e51267e19d2e9eb88f4facbd194
Jefferson Mourent <jasonmourent@gmail.com>
2024-05-17 10:23:49 +0700

    Initial Commit

---
+++ MC2/MC2App.swift
@@ -0,0 +1,17 @@
+//
+//  MC2App.swift
+//  MC2
+//
+//  Created by Jefferson Mourent on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
