a31cc8afb033a05599f85e364bee338ffa1cecfb
Jefferson Mourent <jasonmourent@gmail.com>
2024-05-22 08:20:40 +0700

    Camera view

---
+++ MC2/CameraViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CameraViewController.swift
+//  MC2
+//
+//  Created by Jefferson Mourent on 20/05/24.
+//
+
+import Foundation
---
+++ MC2/CoreML.swift
@@ -0,0 +1,8 @@
+//
+//  CoreML.swift
+//  MC2
+//
+//  Created by Jefferson Mourent on 21/05/24.
+//
+
+import Foundation
