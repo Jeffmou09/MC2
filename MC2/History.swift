2170aad24b368fd5a9ef8932f52866bedd1d186c
Jefferson Mourent <jasonmourent@gmail.com>
2024-05-28 10:35:28 +0700

    Design View

---
+++ MC2/History.swift
@@ -0,0 +1,18 @@
+//
+//  History.swift
+//  MC2
+//
+//  Created by Jefferson Mourent on 28/05/24.
+//
+
+import SwiftUI
+
+struct History: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    History()
+}
