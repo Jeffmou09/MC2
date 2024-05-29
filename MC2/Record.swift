73a6341522e648674028392c2312da58ca02b9e1
Jefferson Mourent <jasonmourent@gmail.com>
2024-05-29 10:17:04 +0700

    Record Video

---
+++ MC2/Record.swift
@@ -0,0 +1,18 @@
+//
+//  Record.swift
+//  MC2
+//
+//  Created by Jefferson Mourent on 29/05/24.
+//
+
+import SwiftUI
+
+struct Record: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Record()
+}
